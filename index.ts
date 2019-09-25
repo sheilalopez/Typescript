f0a7f18bec875464b315c929e5c73b70a7365e8e
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-09-25 11:37:11 +0200

    api

--- index.ts
+++ index.ts
@@ -1,7 +1,36 @@
-import express =  require('express');
+import express = require('express');
 const app: express.Application = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+
+app.get('/all', function (req, res) {
+    let users:{user: string;}[] = [
+        {user:'Toni'},
+        {user:'Joan'}
+    ];
+    res.status(200).json(users);
+});
+
+app.get('/:id', function (req, res) {
+    let id:number = parseInt(req.params.id);
+    if (!Number.isInteger(id)) res.send('Not an integer');
+    else {
+        let body:{id: number, name: string} = {
+            id: id,
+            name: 'lorem'
+        };
+        res.status(200).json(body);
+    }
+});
+
+app.post('/', function (req, res) {
+    res.status(201).json({ user: 'tobi' });
+});
+
+app.put('/:id', function (req, res) {
+    res.status(201).json({ user: 'tobi' });
+});
+
+app.delete('/:id', function (req, res) {
+    res.status(200);
 });
 
 app.listen(3000, function () {
